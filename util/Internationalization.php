66d401144d44acd670af16e210337fa023f56737
marciioluucas <marciioluucas@gmail.com>
2017-04-24 09:03:40 -0300

    Adicionando o resto da documentação no pacote "util";

--- util/Internationalization.php
+++ util/Internationalization.php
@@ -17,7 +17,7 @@ class Internationalization
      * @param $reference
      * @return mixed
      */
-    public static function translate($reference) {
+    public static final function translate($reference) {
         $languageSettedInConfig = JsonReader::read(BASE_DIR.'/phiber_config.json')->phiber->language;
         $lang = JsonReader::read(BASE_DIR."/lang/$languageSettedInConfig.json");
         return $lang->phiber_lang->$reference;
