83f065c4a02bb55752d1886038aac2846c605e91
Diego Brocanelli <diegod2@msn.com>
2018-05-09 18:30:41 -0300

    implementação de organização de código e constante para com o nome do arquivo de configuração.

--- bin/Link.php
+++ bin/Link.php
@@ -12,17 +12,21 @@ use PDO;
 use phiber\util\Internationalization;
 use phiber\util\JsonReader;
 
-
 /**
  * Classe responsável por criar a conexão do banco.
+ * 
  * @package bin
  */
 class Link
 {
+    const PATH_CONFIG_FILE = '/phiber_config.json';
 
+    /**
+     * Undocumented variable
+     *
+     * @var [type]
+     */
     private $instancia;
-
-
 
     /**
      * Função responsável por fazer a conexão com o banco.
@@ -33,25 +37,32 @@ class Link
         try {
             if ($this->instancia == null) {
 
-                $json = new JsonReader(BASE_DIR . "/phiber_config.json");
+                $json = new JsonReader(BASE_DIR . self::PATH_CONFIG_FILE);
 
-                if (!empty(glob(dirname(__DIR__, 4) . "/phiber_config.json")[0])) {
-                    $json = new JsonReader(glob(dirname(__DIR__, 4) . "/phiber_config.json")[0]);
+                if (!empty(glob(dirname(__DIR__, 4) . self::PATH_CONFIG_FILE )[0])) {
+                    $json = new JsonReader(glob(dirname(__DIR__, 4) . PATH_CONFIG_FILE)[0]);
                 }
 
                 $json= $json->read();
                 try {
+
+                    $connectionCache = $json->phiber->link->connection_cache == 1 ? true : false;
+
                     $this->instancia = new PDO(
                         $json->phiber->link->url,
                         $json->phiber->link->user,
                         $json->phiber->link->password,
-                        array(PDO::ATTR_PERSISTENT => $json->phiber->link->connection_cache == 1 ? true : false));
+                        array(
+                            PDO::ATTR_PERSISTENT => $connectionCache
+                        )
+                    );
                 } catch (PhiberException $e) {
                     throw new PhiberException(new Internationalization("database_connection_error"));
                 }
             }
 
             return $this->instancia;
+
         } catch (PhiberException $e) {
             throw new PhiberException(new Internationalization("database_connection_error"));
         }
