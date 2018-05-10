69130ba4b357f6ce84df9b90260487246f5ea700
Diego Brocanelli <diegod2@msn.com>
2018-05-10 18:49:20 -0300

    implementação de quebra de linha nos doc blocks e alinhamento de variáveis.

--- util/Internationalization.php
+++ util/Internationalization.php
@@ -1,35 +1,44 @@
 <?php
+
 /**
  * Copyright (c) 2017. Este código foi feito por @marciioluucas, sob licença MIT
  */
-
 namespace phiber\util;
 
 /**
  * Classe responsável pela internacionalização do projeto.
+ * 
  * @package util
  */
 class Internationalization
 {
-
-
+    /**
+     * Mensagem traduzida.
+     *
+     * @var string
+     */
     private $msgTranslated;
 
     /**
      * Usa a referencia de linguagem no arquivo json para traduzir.
-     * @param String $reference
+     * 
+     * @param string $reference
      */
     public function __construct(String $reference)
     {
         $jsonReader = new JsonReader(BASE_DIR . '/phiber_config.json');
-        $lang = new JsonReader(BASE_DIR . "/lang/" . $jsonReader->read()->phiber->language . ".json");
+        $lang       = new JsonReader(BASE_DIR . "/lang/" . $jsonReader->read()->phiber->language . ".json");
+        
         $this->msgTranslated = $lang->read()->phiber_lang->$reference . "";
     }
 
+    /**
+     * Retornar a mensagem traduzida.
+     *
+     * @return string
+     */
     public function __toString()
     {
         return $this->msgTranslated;
     }
-
-
 }
