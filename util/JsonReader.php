06439eb9ef3a533e77f619fbf5e8cd0f1e7aaaa1
Diego Brocanelli <diegod2@msn.com>
2018-05-10 18:55:21 -0300

    implementão de PSR, alteração de nome de variáveis e alinhamento de código.

--- util/JsonReader.php
+++ util/JsonReader.php
@@ -1,33 +1,45 @@
 <?php
+
 /**
  * Copyright (c) 2017. Este código foi feito por @marciioluucas, sob licença MIT
  */
-
 namespace phiber\util;
 
 /**
  * Classe responsável por ler o arquivo de json especificado.
- * Class JsonReader
+ * 
  * @package util
  */
 class JsonReader
 {
-
-    private $arquivo;
+    /**
+     * Caminho absoluto para o local do arquivo.
+     *
+     * @var string
+     */
+    private $file;
 
     /**
      * JsonReader constructor.
-     * @param $arquivo
+     * 
+     * @param string $file
      */
-    public function __construct($arquivo)
+    public function __construct($file)
     {
-        $this->arquivo = $arquivo;
+        $this->file = $file;
     }
 
-    public function read() {
-        $info = file_get_contents($this->arquivo);
-        $lendo = json_decode($info);
-        return $lendo;
+    /**
+     * Responsável por retornar o conteúdo do arquivo.
+     *
+     * @return array
+     */
+    public function read() 
+    {
+        $content  = file_get_contents($this->file);
+        
+        $read = json_decode($content);
+        
+        return $read;
     }
-
 }
