f34d4800e000d41ed2a0ad01ac13059faa1246d1
Diego Brocanelli <diegod2@msn.com>
2018-05-10 10:33:39 -0300

    inclusão de quebra de linha.

--- util/FuncoesDatas.php
+++ util/FuncoesDatas.php
@@ -1,29 +1,31 @@
 <?php
+
 /**
  * Copyright (c) 2017. Este código foi feito por @marciioluucas, sob licença MIT
  */
-
 namespace phiber\util;
 
 /**
  * Class FuncoesDatas
+ * 
  * @package util
  */
 class FuncoesDatas
 {
-
     /**
      * Converte para data Brasileira.
+     * 
      * @param $data
      * @return false|string
      */
     final public static function converterDataParaBrasileira($data)
     {
-        return date('d-m-Y', strtotime($data));
+        return date( 'd-m-Y', strtotime($data) );
     }
 
     /**
      * Converte para data por extenso Brasileira.
+     * 
      * @param $data
      * @return string
      */
@@ -31,6 +33,7 @@ class FuncoesDatas
     {
         setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
         date_default_timezone_set('America/Sao_Paulo');
-        return strftime('%A, %d de %B de %Y', strtotime($data));
+        
+        return strftime('%A, %d de %B de %Y', strtotime($data) );
     }
 }
